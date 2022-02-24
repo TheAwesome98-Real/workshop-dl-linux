13c147960d8676ff25ddffc9fddd7cb7bf153c12
h110m <50122507+h110m@users.noreply.github.com>
2022-02-24 10:03:42 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SteamWorkshopDownloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                Console.WriteLine(args[0]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
